09af25bd9b80148edaeaad8f777bb0461b721921
Kishorkumar_K <kishorkumar.kumaran@gmail.com>
2020-06-25 17:47:49 +0530

    Program to print a pattern with numbers in a rectangle manner

---
+++ Rectangle_sized_Number_Pattern.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n=5;
+    for(i=n;i>=1;i--,printf("\n"))
+    {
+        for(j=n;j>i;j--)
+            printf("%d",j);
+        for(j=1;j<=i*2-1;j++)
+            printf("%d",i);
+        for(j=i+1;j<=n;j++)
+            printf("%d",j);
+    }
+    for(i=1;i<n;i++,printf("\n"))
+    {
+        for(j=n;j>i;j--)
+            printf("%d",j);
+         for(j=1;j<=i*2-1;j++)
+            printf("%d",i+1);
+        for(j=i+1;j<=n;j++)
+            printf("%d",j);
+    }
+}
