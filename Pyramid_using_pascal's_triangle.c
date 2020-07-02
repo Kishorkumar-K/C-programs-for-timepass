4ab8d2d663d644c342cab7a9f56e6ef09d30e4c5
Kishorkumar <kishorkumar.kumaran@gmail.com>
2020-07-02 19:22:11 +0530

    Program to create a pascals triangle using the entered number as number of rows

---
+++ Pyramid_using_pascal's_triangle.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    int k[10][10];
+    printf("enter n value \n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++,printf("\n"))
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(i==1||j==1||j==i)
+            {
+                k[i][j]=1;
+
+            }
+            else
+            {
+                k[i][j]=k[i-1][j-1]+k[i-1][j];
+
+
+            }
+           printf("%d",k[i][j]);
+        }
+    }
+    return 0;
+}
